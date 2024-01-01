99f1977a9393b7624b6e0c3248b10dbbe4db3bc5
Muhamad Rehan <qamarzaman.comkhan786@gmail.com>
2024-01-01 22:15:17 +0500

    = AC working now

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-let inputDisplay=document.getElementById("result");
-let inputDisplayValue=inputDisplay.value;
-console.log(inputDisplayValue)
+let inputDisplay = document.getElementById("result");
+let inputDisplayValue = inputDisplay.value;
+let cancell = document.getElementById("cancell");
+let equal=document.getElementById("equal")
+let clear=document.getElementById("AC")
+console.log(cancell);
 
-    let btn=document.querySelectorAll(".btn");
-    // console.log(btn);
-    btn.forEach(bt => {
-        bt.addEventListener("click", (e)=>{
-           inputDisplay.value +=(e.target.innerHTML)
-        })
-    });
-    
+let btn = document.querySelectorAll(".btn");
+// console.log(btn);
+btn.forEach((bt) => {
+  bt.addEventListener("click", (e) => {
+    inputDisplay.value += e.target.innerHTML;
+  });
+});
+
+// console.log(cancell)
+cancell.addEventListener("click", () => {
+  inputDisplay.value = inputDisplay.value.slice(0, -1);
+});
+
+equal.addEventListener("click", ()=>inputDisplay.value=eval(inputDisplay.value));
+
+clear.addEventListener("click", () => inputDisplay.value="")
